845af24d5bb6e0af6b1a8f43a733b1196529e115
liweiwei1419 <liweiwei1419@gmail.com>
2025-06-15 18:47:06 +0800

    添加了微信小程序的测试方法。

--- src/main/java/com/suanfa8/algocrazyapi/controller/HelloWorldController.java
+++ src/main/java/com/suanfa8/algocrazyapi/controller/HelloWorldController.java
@@ -4,8 +4,11 @@ import com.suanfa8.algocrazyapi.entity.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @Tag(name = "项目测试")
 @RequestMapping("/hello")
@@ -20,8 +23,9 @@ public class HelloWorldController {
 
     @Operation(summary = "测试 hello world")
     @GetMapping("/world")
-    public String world() {
-        return "hello algo crazy";
+    public String world(@RequestBody Map<String, String> request) {
+        String name = request.get("name");
+        return "hello " + name;
     }
 
 }
