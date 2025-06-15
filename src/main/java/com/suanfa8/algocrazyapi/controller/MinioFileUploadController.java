d23126c582e0caefcf267a9691896239682bc3e6
liweiwei1419 <liweiwei1419@gmail.com>
2025-06-15 17:12:49 +0800

    开放了 minio 接口。

---
+++ src/main/java/com/suanfa8/algocrazyapi/controller/MinioFileUploadController.java
@@ -0,0 +1,59 @@
+package com.suanfa8.algocrazyapi.controller;
+
+import com.suanfa8.algocrazyapi.common.Result;
+import com.suanfa8.algocrazyapi.utils.MinioUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description minio 文件上传控制器
+ */
+@CrossOrigin
+@RestController
+@RequestMapping("/file")
+public class MinioFileUploadController {
+
+    @Autowired
+    private MinioUtils minioUtils;
+
+    /**
+     * @param file     文件
+     * @param fileName 文件名称
+     * @Description 上传文件
+     */
+    @GetMapping("/upload")
+    public Result<String> uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("fileName") String fileName) {
+        minioUtils.upload(file, fileName);
+        return Result.success("上传成功");
+    }
+
+    /**
+     * @param fileName 文件名称
+     * @Description download 文件
+     */
+    @GetMapping("/download")
+    public ResponseEntity downloadFile(@RequestParam("fileName") String fileName) {
+        return minioUtils.download(fileName);
+    }
+
+    /**
+     * @param fileName 文件名称
+     * @Description 得到文件url
+     */
+    @GetMapping("/getUrl")
+    public Result<Map<String, String>> getFileUrl(@RequestParam("fileName") String fileName) {
+        Map<String, String> map = new HashMap<>();
+        map.put("FileUrl", minioUtils.getFileUrl(fileName));
+        return Result.success(map);
+    }
+
+}
