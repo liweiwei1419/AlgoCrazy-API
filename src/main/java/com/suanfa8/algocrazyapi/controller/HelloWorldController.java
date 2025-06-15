5993db6ca83c7d7b1b5fbdf13b4646d00f984dd1
liweiwei1419 <liweiwei1419@gmail.com>
2025-06-15 18:48:54 +0800

    添加微信小程序的测试方法。

--- src/main/java/com/suanfa8/algocrazyapi/controller/HelloWorldController.java
+++ src/main/java/com/suanfa8/algocrazyapi/controller/HelloWorldController.java
@@ -4,6 +4,8 @@ import com.suanfa8.algocrazyapi.entity.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +25,12 @@ public class HelloWorldController {
 
     @Operation(summary = "测试 hello world")
     @GetMapping("/world")
+    public String world() {
+        return "Hello from SpringBoot!";
+    }
+
+
+    @PostMapping ("/greet")
     public String world(@RequestBody Map<String, String> request) {
         String name = request.get("name");
         return "hello " + name;
