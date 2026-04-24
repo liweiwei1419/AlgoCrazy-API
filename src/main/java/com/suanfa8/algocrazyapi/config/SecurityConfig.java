17be1e2ed9f53b0fa19ffcaa0e1e6a516e098f81
liweiwei1419 <liweiwei1419@gmail.com>
2026-04-25 02:36:17 +0800

    Security 放行的路径恢复。

--- src/main/java/com/suanfa8/algocrazyapi/config/SecurityConfig.java
+++ src/main/java/com/suanfa8/algocrazyapi/config/SecurityConfig.java
@@ -36,9 +36,6 @@ public class SecurityConfig {
 
     private static final String[] URL_WHITELIST = {
             // 放行的 URL 路径
-            "/exercise-solutions/**",
-
-
             "/auth/authenticate",
             // 添加退出登录接口路径
             "/comment/comments", "/comment/add", "/comment/*/replies", "/user/register", "/user/forgot-password", "/user/reset-password", "/user/homepage/**", "/hello/world", "/hello/days-until-2025-10-30", "/hello/greet", "/tree/**", "/article/**", "/file/**", "/leetcode/problems/**", "/message/**", "/algorithm-category/**",
@@ -49,7 +46,6 @@ public class SecurityConfig {
             "/webjars/**",           // Swagger 可能需要的静态资源
             "/swagger-resources/**",    // Swagger 资源
             "/doc.html"     // Knife4j 主页
-
     };
 
     /**
